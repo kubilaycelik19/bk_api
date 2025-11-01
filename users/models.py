d9a396191de7fe04c1078abaf905971d7c3dd367
kubilay Celik <celikkubilay4006@gmail.com>
2025-11-01 23:35:02 +0300

    FIX: (ZORLA GÖNDERME) CustomUserManager ve TÜM migration'ları pushla

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import BaseUserManager
 
+# GÖREV 77: DEĞİŞİKLİĞİ ZORLA (DUMMY COMMENT)
 
 class CustomUserManager(BaseUserManager):
     """
