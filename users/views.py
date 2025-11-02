fc7277abb880915f2b42fa1886e74cd9a6f35d6e
kubilay Celik <celikkubilay4006@gmail.com>
2025-11-02 13:34:37 +0300

    DOCS: Add .env.example file for environment variabless

--- users/views.py
+++ users/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status, permissions
 from .models import CustomUser
 from .serializers import UserSerializer
 from django.contrib.auth.hashers import make_password
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny, BasePermission
 
 class UserViewSet(viewsets.ModelViewSet):
 
